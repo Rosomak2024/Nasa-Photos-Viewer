0115f3a43d844732757e4bb1dc52bfb5439d3d97
Rosomak2024 <mbnet@o2.pl>
2024-03-15 11:47:38 +0100

    First html and css structure added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Nasa Photos Viewer</h1>
+      {/* <Main></Main>
+      <Footer></Footer> */}
+    </>
+  );
+}
+
+export default App;
