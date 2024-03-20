fffb9b4feccb0e5bc8700c33a567660ff4823157
Rosomak2024 <mbnet@o2.pl>
2024-03-20 12:27:07 +0100

    problem with git fixed

--- src/View.jsx
+++ src/View.jsx
@@ -2,47 +2,50 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const View = () => {
-  const [photoData, setPhotoData] = useState(null);
+  const [photoData, setPhotoData] = useState({});
   const [selectedDate, setSelectedDate] = useState("");
 
   useEffect(() => {
-    const apiUrl = selectedDate
-      ? `https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ&date=${selectedDate}` // przekazuje &date=${selectedDate}
-      : `https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ`;
+    let apiUrl = "";
+    if (selectedDate) {
+      apiUrl = `https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ&date=${selectedDate}`;
+    } else {
+      apiUrl = `https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ`;
+    }
+
     axios
       .get(apiUrl)
       .then((response) => {
         setPhotoData(response.data);
-        console.log("response.data", response.data);
       })
       .catch((error) => {
         console.error("Error download:", error);
       });
   }, [selectedDate]);
 
-  const handleDateChange = (e) => {
+  const handleSelectedChange = (e) => {
     setSelectedDate(e.target.value);
+    console.log("e.target.value", e.target.value);
   };
 
   return (
     <div>
       {photoData ? (
         <>
-          <h2>{photoData.title}</h2>
-          <img src={photoData.url} alt={photoData.title} />
-          <p className="explanation-inform">
-            {photoData.explanation}
-            {photoData.date}
-          </p>
-          <p className="date-inform">{photoData.date}</p>
-
           <input
             type="date"
             id="start"
             name="trip-start"
-            onChange={handleDateChange}
+            onChange={handleSelectedChange}
             value={selectedDate}
           />
+
+          <div className="photo">
+            <img src={photoData.url} alt={photoData.title} />
+            <h3>{photoData.title}</h3>
+            <p>{photoData.explanation}</p>
+            <p>{photoData.date}</p>
+          </div>
         </>
       ) : (
         <p>Loading...</p>
